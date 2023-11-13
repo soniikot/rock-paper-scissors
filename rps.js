716398e39383dc66d70e3760974deb1fd257a045
soniikot <soniikot@gmail.com>
2023-11-12 17:20:06 -0800

    add playRound function

--- rps.js
+++ rps.js
@@ -1,11 +1,34 @@
-let playerSelection = getPlayerChoice();
-let computerSelection =getComputerChoice();
-let playerScore = 0;
-let computerScore = 0;
+const playerText = document.querySelector('#playerChoice');
+const computerText = document.querySelector('#computerChoice');
+const resultText = document.querySelector('#result');
+const buttons = document.querySelectorAll('.buttons');
+ let playerSelection;
+ let computerSelection;
+let result;
+
+buttons.forEach(button => button.addEventListener('click',()=>{
+  playerSelection =button.textContent;
+  getComputerChoice();
+  playerText.textContent = `Player: ${playerSelection}`;
+  computerText.textContent = `Computer: ${computerSelection}`;
+  resultText.textContent = playRound();
+
+}))
+function getComputerChoice(){
+  let choice = ['rock', 'paper','scissors'];
+  computerSelection = choice[Math.floor(Math.random() * 3)];
+  return computerSelection;
+  }
+  
+
+//let playerSelection = getPlayerChoice();
+//let computerSelection =getComputerChoice();
+//let playerScore = 0;
+//let computerScore = 0;
 
 
 
-function handleClick(event) {
+/*function handleClick(event) {
   if (event.target.tagName !== "BUTTON") {
     return;
   }
@@ -17,22 +40,15 @@ function handleClick(event) {
 document
   .querySelector(".buttons")
   .addEventListener("click", handleClick);
-
+*/
   
 
 
-function getComputerChoice(){
-let choice = ['rock', 'paper','scissors'];
-choice = choice[Math.floor(Math.random() * 3)];
-return choice;
-}
 
 
 
 
-
-
-function playRound(computerSelection, playerSelection) {
+function playRound() {
   
   
  if (playerSelection == computerSelection) {
@@ -43,20 +59,20 @@ function playRound(computerSelection, playerSelection) {
 else if(computerSelection == "rock" && playerSelection =="paper" ||
 computerSelection == "paper" && playerSelection =="scissors" ||
 computerSelection == "scissors" && playerSelection =="rock"){
-   playerScore ++;
+  // playerScore ++;
     return  "You Win!";
 }
   
-   else if (playerSelection!=("rock" || "paper" || "scissors")){
+  /* else if (playerSelection!=("rock" || "paper" || "scissors")){
     return "Check you selection";
-  }
+  }*/
     else {
-        computerScore++;
+       // computerScore++;
         return "You Lose";
        }
        }
 
-    function game(){
+    /*function game(){
      for (let round = 1; round <= 5; round++) {
        let playerSelection = getPlayerChoice();
         let computerSelection = getComputerChoice();
@@ -77,5 +93,5 @@ computerSelection == "scissors" && playerSelection =="rock"){
 
 }
 
-game()
+game()*/
 
