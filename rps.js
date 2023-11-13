10eaa4b4cd3b79318367dadf6dba7abf682f415c
soniikot <soniikot@gmail.com>
2023-11-13 13:06:33 -0800

    add checkwinnerfunction

--- rps.js
+++ rps.js
@@ -1,97 +1,94 @@
+let playerSelection;
+let computerSelection;
+let result;
+let playerScore = 0;
+let computerScore = 0;
+let round = 1;
+
+const roundText = document.createElement('h1');
 const playerText = document.querySelector('#playerChoice');
 const computerText = document.querySelector('#computerChoice');
 const resultText = document.querySelector('#result');
 const buttons = document.querySelectorAll('.buttons');
- let playerSelection;
- let computerSelection;
-let result;
+const container = document.querySelector('#container');
+const computerScoreText = document.createElement('p');
+const playerScoreText = document.createElement('p');
+const finalText = document.createElement('p');
 
-buttons.forEach(button => button.addEventListener('click',()=>{
-  playerSelection =button.textContent;
-  getComputerChoice();
-  playerText.textContent = `Player: ${playerSelection}`;
-  computerText.textContent = `Computer: ${computerSelection}`;
-  resultText.textContent = playRound();
+container.appendChild(roundText);
+container.appendChild(computerScoreText);
+container.appendChild(playerScoreText);
+container.appendChild(finalText);
 
-}))
-function getComputerChoice(){
-  let choice = ['rock', 'paper','scissors'];
+
+
+  buttons.forEach(button => button.addEventListener('click', () => {
+
+    roundText.textContent = `Round ${round}`;
+    playerSelection = button.textContent;
+    getComputerChoice();
+    playerText.textContent = `Player: ${playerSelection}`;
+    computerText.textContent = `Computer: ${computerSelection}`;
+
+    resultText.textContent = playRound();
+    
+  round++;
+  
+    
+   checkWinner();
+   
+  }
+    
+
+  
+  
+  ))
+
+
+function getComputerChoice() {
+  let choice = ['rock', 'paper', 'scissors'];
   computerSelection = choice[Math.floor(Math.random() * 3)];
   return computerSelection;
-  }
-  
-
-//let playerSelection = getPlayerChoice();
-//let computerSelection =getComputerChoice();
-//let playerScore = 0;
-//let computerScore = 0;
-
-
-
-/*function handleClick(event) {
-  if (event.target.tagName !== "BUTTON") {
-    return;
-  }
-  let playerChoice = event.target.innerText;
-  document
-    .querySelector("#result")
-    .innerText = playerChoice;
 }
-document
-  .querySelector(".buttons")
-  .addEventListener("click", handleClick);
-*/
-  
-
-
-
-
 
 
 function playRound() {
-  
-  
- if (playerSelection == computerSelection) {
-        return "Draw";
- }
-  
- 
-else if(computerSelection == "rock" && playerSelection =="paper" ||
-computerSelection == "paper" && playerSelection =="scissors" ||
-computerSelection == "scissors" && playerSelection =="rock"){
-  // playerScore ++;
-    return  "You Win!";
-}
-  
-  /* else if (playerSelection!=("rock" || "paper" || "scissors")){
-    return "Check you selection";
-  }*/
+  {
+
+
+    if (playerSelection == computerSelection) {
+      return "Draw";
+    }
+
+
+    else if (computerSelection == "rock" && playerSelection == "paper" ||
+      computerSelection == "paper" && playerSelection == "scissors" ||
+      computerSelection == "scissors" && playerSelection == "rock") {
+      playerScore = playerScore + 1;
+      playerScoreText.textContent = `Your score: ${playerScore}`;
+
+      return "You Win!";
+    }
+
+
     else {
-       // computerScore++;
-        return "You Lose";
-       }
-       }
-
-    /*function game(){
-     for (let round = 1; round <= 5; round++) {
-       let playerSelection = getPlayerChoice();
-        let computerSelection = getComputerChoice();
-
-        let roundResult = playRound(computerSelection, playerSelection);
-        console.log('You chooses: ' + playerSelection);
-        console.log('Computer chooses: ' + computerSelection);
-        console.log('Round ' + round + ': ' + roundResult);
+      computerScore = computerScore + 1;
+      computerScoreText.textContent = `Computer score: ${computerScore}`;
+      return "You Lose";
     }
-
-    if (playerScore > computerScore) {
-        console.log('You win! Your Score: ' + playerScore + ' Computer Score: ' + computerScore);
-    } else if (playerScore < computerScore) {
-        console.log('You lose! Your Score: ' + playerScore + ' Computer Score: ' + computerScore);
-    } else {
-        console.log('Draw! Your Score: ' + playerScore + ' Computer Score: ' + computerScore);
-    }
-
+  }
 }
 
-game()*/
 
+function checkWinner() {
+  {
+    if (playerScore > computerScore) {
+      finalText.textContent = 'You are the best!';
+    } else if (playerScore < computerScore) {
+      finalText.textContent = 'You win next time';
+    } else {
+      finalText.textContent = 'You and computer are both good!';
+    }
+  }
+
+}
