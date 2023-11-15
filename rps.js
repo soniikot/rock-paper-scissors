1f40d3f2a93076add7f3efc3f628f82199d2068d
soniikot <soniikot@gmail.com>
2023-11-14 19:45:18 -0800

    disable button add round count

--- rps.js
+++ rps.js
@@ -5,7 +5,7 @@ let playerScore = 0;
 let computerScore = 0;
 let round = 1;
 
-const roundText = document.createElement('h1');
+const roundText = document.querySelector('#round');
 const playerText = document.querySelector('#playerChoice');
 const computerText = document.querySelector('#computerChoice');
 const resultText = document.querySelector('#result');
@@ -14,12 +14,11 @@ const container = document.querySelector('#container');
 const computerScoreText = document.createElement('p');
 const playerScoreText = document.createElement('p');
 const finalText = document.createElement('p');
+const restartBtn = document.createElement('button');
 
-container.appendChild(roundText);
 container.appendChild(computerScoreText);
 container.appendChild(playerScoreText);
 container.appendChild(finalText);
-
 
 
   buttons.forEach(button => button.addEventListener('click', () => {
@@ -36,13 +35,20 @@ container.appendChild(finalText);
   
     
    checkWinner();
+   stopGame();
    
   }
-    
-
+    ))
+  restartBtn.addEventListener('click', () => {
+    round=1;
+    playerScore=0;
+    computerScore=0;
+ playerText.textContent = `Player: ${playerSelection}`;
+    computerText.textContent = `Computer: ${computerSelection}`;
+    playerScoreText.textContent = `Your score: ${playerScore}`;
+    computerScoreText.textContent = `Computer score: ${computerScore}`;
   
-  
-  ))
+  })
 
 
 function getComputerChoice() {
@@ -66,6 +72,7 @@ function playRound() {
       computerSelection == "scissors" && playerSelection == "rock") {
       playerScore = playerScore + 1;
       playerScoreText.textContent = `Your score: ${playerScore}`;
+      
 
       return "You Win!";
     }
@@ -81,14 +88,31 @@ function playRound() {
 
 
 function checkWinner() {
-  {
-    if (playerScore > computerScore) {
-      finalText.textContent = 'You are the best!';
-    } else if (playerScore < computerScore) {
-      finalText.textContent = 'You win next time';
-    } else {
-      finalText.textContent = 'You and computer are both good!';
-    }
+  if (playerScore==5){
+     finalText.textContent = 'You won the machine 5 times';
   }
+  else if (computerScore==5){
+        finalText.textContent = 'Computer won! You get lucky next time';
+      } 
+     
+    }
 
-}
+    function stopGame(){
+      if (playerScore==5||computerScore==5){
+        document.querySelectorAll(".buttons").forEach(e => e.disabled = true)
+        const restartBtn = document.createElement('button');
+        restartBtn.textContent = 'Start over';
+        container.appendChild(restartBtn);
+
+      }
+    }
+
+
+function stopGame(){
+  if (playerScore==5||computerScore==5){
+    document.querySelectorAll(".buttons").forEach(e => e.disabled = true)
+    
+    restartBtn.textContent = 'Start over';
+    container.appendChild(restartBtn);
+
+  }}
